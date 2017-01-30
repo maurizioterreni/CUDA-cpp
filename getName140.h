44f15592bf83b754203969ed5e7f48fe164c6d06
maurizioterreni <maurizio.terreni@live.it>
2017-01-30 10:26:20 +0100

    Change

---
+++ getName140.h
@@ -0,0 +1,425 @@
+std::string getName140(unsigned long val){
+	std::string str;
+	if(val == 16770273){
+		str = "MistyRose";
+	}
+	if(val == 10145074){
+		str = "YellowGreen";
+	}
+	if(val == 16711680){
+		str = "Red";
+	}
+	if(val == 12211667){
+		str = "MediumOrchid";
+	}
+	if(val == 16737095){
+		str = "Tomato";
+	}
+	if(val == 12632256){
+		str = "Silver";
+	}
+	if(val == 9419919){
+		str = "DarkSeaGreen";
+	}
+	if(val == 2263842){
+		str = "ForestGreen";
+	}
+	if(val == 14329120){
+		str = "GoldenRod";
+	}
+	if(val == 15787660){
+		str = "Khaki";
+	}
+	if(val == 14204888){
+		str = "Thistle";
+	}
+	if(val == 13882323){
+		str = "LightGrey";
+	}
+	if(val == 4772300){
+		str = "MediumTurquoise";
+	}
+	if(val == 6970061){
+		str = "SlateBlue";
+	}
+	if(val == 16738740){
+		str = "HotPink";
+	}
+	if(val == 16448210){
+		str = "LightGoldenRodYellow";
+	}
+	if(val == 14381203){
+		str = "PaleVioletRed";
+	}
+	if(val == 4734347){
+		str = "DarkSlateBlue";
+	}
+	if(val == 9662683){
+		str = "MediumPurple";
+	}
+	if(val == 9109504){
+		str = "DarkRed";
+	}
+	if(val == 16752762){
+		str = "LightSalmon";
+	}
+	if(val == 16445670){
+		str = "Linen";
+	}
+	if(val == 11591910){
+		str = "PowderBlue";
+	}
+	if(val == 2003199){
+		str = "DodgerBlue";
+	}
+	if(val == 16777184){
+		str = "LightYellow";
+	}
+	if(val == 8087790){
+		str = "MediumSlateBlue";
+	}
+	if(val == 16316671){
+		str = "GhostWhite";
+	}
+	if(val == 15794160){
+		str = "HoneyDew";
+	}
+	if(val == 8388564){
+		str = "Aquamarine";
+	}
+	if(val == 8900331){
+		str = "SkyBlue";
+	}
+	if(val == 65535){
+		str = "Cyan";
+	}
+	if(val == 11119017){
+		str = "DarkGrey";
+	}
+	if(val == 16643558){
+		str = "OldLace";
+	}
+	if(val == 16711935){
+		str = "Magenta";
+	}
+	if(val == 16119285){
+		str = "WhiteSmoke";
+	}
+	if(val == 14745599){
+		str = "LightCyan";
+	}
+	if(val == 16113331){
+		str = "Wheat";
+	}
+	if(val == 2142890){
+		str = "LightSeaGreen";
+	}
+	if(val == 6737322){
+		str = "MediumAquaMarine";
+	}
+	if(val == 3100495){
+		str = "DarkSlateGrey";
+	}
+	if(val == 12357519){
+		str = "RosyBrown";
+	}
+	if(val == 9498256){
+		str = "LightGreen";
+	}
+	if(val == 32896){
+		str = "Teal";
+	}
+	if(val == 13047173){
+		str = "MediumVioletRed";
+	}
+	if(val == 16770229){
+		str = "Moccasin";
+	}
+	if(val == 128){
+		str = "Navy";
+	}
+	if(val == 16776960){
+		str = "Yellow";
+	}
+	if(val == 15132410){
+		str = "Lavender";
+	}
+	if(val == 11393254){
+		str = "LightBlue";
+	}
+	if(val == 9109643){
+		str = "DarkMagenta";
+	}
+	if(val == 32768){
+		str = "Green";
+	}
+	if(val == 10040012){
+		str = "DarkOrchid";
+	}
+	if(val == 255){
+		str = "Blue";
+	}
+	if(val == 6591981){
+		str = "CornflowerBlue";
+	}
+	if(val == 15657130){
+		str = "PaleGoldenRod";
+	}
+	if(val == 6908265){
+		str = "DimGrey";
+	}
+	if(val == 16744272){
+		str = "Coral";
+	}
+	if(val == 16753920){
+		str = "Orange";
+	}
+	if(val == 16032864){
+		str = "SandyBrown";
+	}
+	if(val == 16416882){
+		str = "Salmon";
+	}
+	if(val == 35723){
+		str = "DarkCyan";
+	}
+	if(val == 65407){
+		str = "SpringGreen";
+	}
+	if(val == 16119260){
+		str = "Beige";
+	}
+	if(val == 5597999){
+		str = "DarkOliveGreen";
+	}
+	if(val == 6697881){
+		str = "RebeccaPurple";
+	}
+	if(val == 13458524){
+		str = "IndianRed ";
+	}
+	if(val == 9055202){
+		str = "BlueViolet";
+	}
+	if(val == 16770244){
+		str = "Bisque";
+	}
+	if(val == 14474460){
+		str = "Gainsboro";
+	}
+	if(val == 65280){
+		str = "Lime";
+	}
+	if(val == 14524637){
+		str = "Plum";
+	}
+	if(val == 10506797){
+		str = "Sienna";
+	}
+	if(val == 16444375){
+		str = "AntiqueWhite";
+	}
+	if(val == 9127187){
+		str = "SaddleBrown";
+	}
+	if(val == 11674146){
+		str = "FireBrick";
+	}
+	if(val == 16773365){
+		str = "LavenderBlush";
+	}
+	if(val == 139){
+		str = "DarkBlue";
+	}
+	if(val == 16766720){
+		str = "Gold";
+	}
+	if(val == 16775388){
+		str = "Cornsilk";
+	}
+	if(val == 11584734){
+		str = "LightSteelBlue";
+	}
+	if(val == 52945){
+		str = "DarkTurquoise";
+	}
+	if(val == 25600){
+		str = "DarkGreen";
+	}
+	if(val == 13808780){
+		str = "Tan";
+	}
+	if(val == 10824234){
+		str = "Brown";
+	}
+	if(val == 16777215){
+		str = "White";
+	}
+	if(val == 3978097){
+		str = "MediumSeaGreen";
+	}
+	if(val == 16775920){
+		str = "FloralWhite";
+	}
+	if(val == 4915330){
+		str = "Indigo  ";
+	}
+	if(val == 3417055){
+		str = "name";
+	}
+	if(val == 0){
+		str = "Black";
+	}
+	if(val == 16121850){
+		str = "MintCream";
+	}
+	if(val == 16772045){
+		str = "BlanchedAlmond";
+	}
+	if(val == 6266528){
+		str = "CadetBlue";
+	}
+	if(val == 8421504){
+		str = "Grey";
+	}
+	if(val == 3329330){
+		str = "LimeGreen";
+	}
+	if(val == 16773077){
+		str = "PapayaWhip";
+	}
+	if(val == 12092939){
+		str = "DarkGoldenRod";
+	}
+	if(val == 16775930){
+		str = "Snow";
+	}
+	if(val == 8388608){
+		str = "Maroon";
+	}
+	if(val == 15761536){
+		str = "LightCoral";
+	}
+	if(val == 11529966){
+		str = "PaleTurquoise";
+	}
+	if(val == 15631086){
+		str = "Violet";
+	}
+	if(val == 13468991){
+		str = "Peru";
+	}
+	if(val == 8388736){
+		str = "Purple";
+	}
+	if(val == 15792383){
+		str = "AliceBlue";
+	}
+	if(val == 8421376){
+		str = "Olive";
+	}
+	if(val == 49151){
+		str = "DeepSkyBlue";
+	}
+	if(val == 7048739){
+		str = "OliveDrab";
+	}
+	if(val == 4620980){
+		str = "SteelBlue";
+	}
+	if(val == 205){
+		str = "MediumBlue";
+	}
+	if(val == 3050327){
+		str = "SeaGreen";
+	}
+	if(val == 16729344){
+		str = "OrangeRed";
+	}
+	if(val == 16775885){
+		str = "LemonChiffon";
+	}
+	if(val == 15308410){
+		str = "DarkSalmon";
+	}
+	if(val == 16767673){
+		str = "PeachPuff";
+	}
+	if(val == 16777200){
+		str = "Ivory";
+	}
+	if(val == 16761035){
+		str = "Pink";
+	}
+	if(val == 8900346){
+		str = "LightSkyBlue";
+	}
+	if(val == 13789470){
+		str = "Chocolate";
+	}
+	if(val == 16758465){
+		str = "LightPink";
+	}
+	if(val == 14423100){
+		str = "Crimson";
+	}
+	if(val == 9699539){
+		str = "DarkViolet";
+	}
+	if(val == 11403055){
+		str = "GreenYellow";
+	}
+	if(val == 15794175){
+		str = "Azure";
+	}
+	if(val == 8388352){
+		str = "Chartreuse";
+	}
+	if(val == 12433259){
+		str = "DarkKhaki";
+	}
+	if(val == 4286945){
+		str = "RoyalBlue";
+	}
+	if(val == 16747520){
+		str = "DarkOrange";
+	}
+	if(val == 7372944){
+		str = "SlateGrey";
+	}
+	if(val == 10025880){
+		str = "PaleGreen";
+	}
+	if(val == 8190976){
+		str = "LawnGreen";
+	}
+	if(val == 16774638){
+		str = "SeaShell";
+	}
+	if(val == 14596231){
+		str = "BurlyWood";
+	}
+	if(val == 7833753){
+		str = "LightSlateGrey";
+	}
+	if(val == 4251856){
+		str = "Turquoise";
+	}
+	if(val == 14315734){
+		str = "Orchid";
+	}
+	if(val == 1644912){
+		str = "MidnightBlue";
+	}
+	if(val == 16768685){
+		str = "NavajoWhite";
+	}
+	if(val == 16716947){
+		str = "DeepPink";
+	}
+	if(val == 64154){
+		str = "MediumSpringGreen";
+	}
+	else{
+ str="unknown";
+}
